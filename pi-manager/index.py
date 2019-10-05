bead4bd4a108f2a5accb2df2a77fb1aa79d3d19a
yangxiaodong <yangxiaodong@qianmi.com>
2019-10-05 23:49:06 +0800

    add: 前轮正常. 后轮可以后退, 不能前进 ..

--- pi-manager/index.py
+++ pi-manager/index.py
@@ -1,2 +1,200 @@
 
-print("hello")
+'''
+在目标机器上执行.
+sudo systemctl enable pigpiod
+或sudo systemctl start pigpiod
+
+# 手工执行
+sudo pigpiod
+
+
+ 本地运行
+  PIGPIO_ADDR=172.19.8.53 python3 index.py
+  PIGPIO_ADDR=192.168.0.112 python3 index.py
+'''
+from gpiozero import Motor, LED, PWMLED
+from time import sleep
+import configparser
+
+from gpiozero.pins.pigpio import PiGPIOFactory
+factory = PiGPIOFactory(host='192.168.0.112', port=8888)
+
+#
+# red = LED(17, factory)
+# #
+# # while True:
+# if True:
+#     print('next')
+#     red.on()
+#     sleep(1)
+#     red.off()
+#     sleep(1)
+#
+
+
+class FourWheelDriveCar():
+    # Define the number of all the GPIO that will used for the 4wd car
+
+    def __init__(self):
+        '''
+        1. Read pin number from configure file
+        2. Init all GPIO configureation
+        '''
+        config = configparser.ConfigParser()
+        config.read("config.ini")
+
+        self.lfMotor = Motor(config.getint("car", "LEFT_FRONT_1"), config.getint("car", "LEFT_FRONT_2"), pin_factory=factory)
+        self.rfMotor = Motor(config.getint("car", "RIGHT_FRONT_1"), config.getint("car", "RIGHT_FRONT_2"), pin_factory=factory)
+        self.rfPwa = PWMLED(config.getint("car", "RIGHT_FRONT__PWA"), pin_factory=factory)
+        self.lfPwa = PWMLED(config.getint("car", "LEFT_FRONT__PWA"), pin_factory=factory)
+        self.forwardFire = LED(config.getint("car", "FORWARD_STBY"), pin_factory=factory)
+
+
+        self.lbMotor = Motor(config.getint("car", "LEFT_BEHIND_1"), config.getint("car", "LEFT_BEHIND_2"), pin_factory=factory)
+        self.rbMotor = Motor(config.getint("car", "RIGHT_BEHIND_1"), config.getint("car", "RIGHT_BEHIND_2"), pin_factory=factory)
+        self.rbPwa = PWMLED(config.getint("car", "RIGHT_BEHIND__PWA"), pin_factory=factory)
+        self.lbPwa = PWMLED(config.getint("car", "LEFT_BEHIND__PWA"), pin_factory=factory)
+        self.behindFire = LED(config.getint("car", "BEHIND_STBY"), pin_factory=factory)
+
+        self.fire_on()
+
+    def fire_on(self):
+        self.forwardFire.on()
+        self.behindFire.on()
+
+    def fire_off(self):
+        self.lfPwa.off()
+        self.rfPwa.off()
+        self.lbPwa.off()
+        self.rbPwa.off()
+        self.forwardFire.off()
+        self.behindFire.off()
+
+    def reset(self):
+        # Rest all the GPIO as LOW
+
+        self.lfPwa.off()
+        self.rfPwa.off()
+        self.lbPwa.off()
+        self.rbPwa.off()
+
+        self.lfMotor.stop()
+        self.rfMotor.stop()
+        self.lbMotor.stop()
+        self.rbMotor.stop()
+
+        self.fire_on()
+
+    def __forword(self):
+        '''
+        前进
+        :return:
+        '''
+        self.reset()
+
+        self.lfMotor.forward()
+        self.lfPwa.pulse(n=5)
+
+        self.rfMotor.forward()
+        self.rfPwa.pulse()
+
+        # self.lbMotor.forward()
+        self.lbMotor.forward()
+        self.lbPwa.pulse()
+
+        self.rbMotor.forward()
+        self.rbPwa.pulse()
+
+    def __backword(self):
+        '''
+        后退
+        :return:
+        '''
+        self.reset()
+
+        self.lfMotor.backward()
+        self.lfPwa.pulse()
+
+        self.rfMotor.backward()
+        self.rfPwa.pulse()
+
+        self.lbMotor.backward()
+        self.lbPwa.pulse()
+
+        self.rbMotor.backward()
+        self.rbPwa.pulse()
+
+    def __turnLeft(self):
+        '''
+        向左转
+        To turn left, the LEFT_FRONT wheel will move backword
+        All other wheels move forword
+        '''
+        self.reset()
+        self.lfMotor.backward()
+        self.rfMotor.forward()
+        self.lbMotor.forward()
+        self.rbMotor.backward()
+
+
+    def __turnRight(self):
+        '''
+        向右转
+        To turn right, the RIGHT_FRONT wheel move backword
+        All other wheels move forword
+        '''
+        self.reset()
+        self.lfMotor.forward()
+        self.rfMotor.backward()
+        self.lbMotor.backward()
+        self.rbMotor.forward()
+
+    def __backLeft(self):
+        '''
+        To go backword and turn left, the LEFT_BEHIND wheel move forword
+        All other wheels move backword
+        '''
+        self.reset()
+        print('TODO __backLeft')
+
+    def __backRight(self):
+        '''
+        To go backword and turn right, the RIGHT_BEHIND wheel move forword
+        All other wheels move backword
+        '''
+        self.reset()
+        print('TODO __backRight')
+
+    def __stop(self):
+        self.reset()
+
+    def carMove(self, direction):
+        '''
+        Car move according to the input paramter - direction
+        '''
+        if direction.lower() == 'w':
+            self.__forword()
+        elif direction.lower() == 's':
+            self.__backword()
+        # elif direction.lower() == 'a':
+        #     self.__turnLeft()
+        # elif direction.lower() == 'd':
+        #     self.__turnRight()
+        # elif direction.lower() == 'BL':
+        #     self.__backLeft()
+        # elif direction.lower() == 'BR':
+        #     self.__backRight()
+        elif direction.lower() == 'stop':
+            self.__stop()
+        elif direction.lower() == 'clear':
+            self.__stop()
+            self.fire_off()
+        else:
+            print("The input direction is wrong! You can just input: F, B, L, R, BL,BR or S, current is ", direction)
+
+
+if __name__ == "__main__":
+    raspCar = FourWheelDriveCar()
+    while (True):
+        direction = input("Please input direction: ")
+        raspCar.carMove(direction)
--- wechat/src/pages/index/index.tsx
+++ wechat/src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import {View} from '@tarojs/components';
 import Taro, {Component} from '@tarojs/taro';
 
 import {connect} from '@tarojs/redux';
+import { AtButton } from 'taro-ui'
 import './index.less';
 import * as T from './types';
 import actions from './actions';
@@ -38,8 +39,96 @@ export default class Index extends Component<Partial<T.IProps>, any> {
   render() {
     return (
       <View className="index">
-        hello
+        <AtButton onClick={()=>{
+          //@ts-ignore
+          wx.openBluetoothAdapter({
+            success: (res) =>{
+              console.log(res)
+              //@ts-ignore
+              // wx.onBluetoothDeviceFound( (res)=> {
+              //   var devices = res.devices;
+              //   console.log('new device list has founded');
+              //   console.dir(devices)
+              //   console.log(ab2hex(devices[0].advertisData))
+              // })
+
+              //@ts-ignore
+              wx.startBluetoothDevicesDiscovery({
+                // services: ['FEE7'],
+                success: (res) =>{
+                  console.log(res);
+                  this.getBluetoothDevices();
+
+                  // wx.getBluetoothDevices({
+                  //   success: function (res) {
+                  //     console.log(res);
+                  //     if (res.devices[0]) {
+                  //       console.log(ab2hex(res.devices[0].advertisData))
+                  //     }
+                  //   }
+                  // })
+
+                },
+                fail:(err)=> {
+                  console.error('调用失败;',err);
+                }
+              })
+            },fail(err){
+              console.error(err);
+            }
+          })
+
+        }}>搜索蓝牙</AtButton>
+        <AtButton>前进</AtButton>
+        <AtButton>后退</AtButton>
+        <AtButton>左移</AtButton>
+        <AtButton>右移</AtButton>
       </View>
     );
   }
+
+  getBluetoothDevices=()=> {
+
+    setTimeout(() => {
+      //@ts-ignore
+      wx.getBluetoothDevices({
+        services: [],
+        allowDuplicatesKey: false,
+        interval: 0,
+        success: function(res) {
+          console.log('devices:',res);
+          if (res.devices.length > 0) {
+            if (JSON.stringify(res.devices).indexOf(that.deviceName) !== -1) {
+              for (let i = 0; i < res.devices.length; i++) {
+                if (that.deviceName === res.devices[i].name) {
+                  /* 根据指定的蓝牙设备名称匹配到deviceId */
+                  that.deviceId = that.devices[i].deviceId;
+                  setTimeout(() => {
+                    that.connectTO();
+                  }, 2000);
+                };
+              };
+            } else {
+            }
+          } else {
+          }
+        },
+        fail(res) {
+          console.log(res, '获取蓝牙设备列表失败=====')
+        }
+      })
+    }, 2000)
+  },
+}
+
+
+// ArrayBuffer转16进度字符串示例
+function ab2hex(buffer) {
+  var hexArr = Array.prototype.map.call(
+    new Uint8Array(buffer),
+    function(bit) {
+      return ('00' + bit.toString(16)).slice(-2)
+    }
+  )
+  return hexArr.join('');
 }
